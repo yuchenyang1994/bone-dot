3d0b9050cc7affb62c3ff1574b21ab4a59c6e053
yuchenyang1994 <yuchenyang1994@foxmail.com>
2025-04-18 16:27:16 +0800

    Add: pillow && mesh & install pillow

--- bone_dot/__init__.py
+++ bone_dot/__init__.py
@@ -1,18 +1,7 @@
 import bpy
-
-import os
 import sys
-
-addon_dir = os.path.dirname(__file__)
-vendor_dir = os.path.join(addon_dir, "libs")
-
-if vendor_dir not in sys.path:
-    sys.path.append(vendor_dir)
-    print(vendor_dir)
-
-from bone_dot.panel import viewport_panel, sprite_panel
-from bone_dot.operator import view2d_operator, sprite_operator, mesh_poerator
-
+import os
+import subprocess
 
 bl_info = {
     "name": "BoneDot",
@@ -27,23 +16,44 @@ bl_info = {
 }
 
 
-classes = (
-    viewport_panel.Bonedot_PT_ViewPortPanel,
-    view2d_operator.Bonedot_OT_SetView2D,
-    # import sprite
-    sprite_panel.Bonedot_PT_ImportSprite,
-    sprite_operator.Bonedot_OT_ImportSprites,
-    sprite_operator.Bonedot_OT_CreateMaterialGroup,
-    sprite_operator.Bonedot_OT_ImportSingleSprite,
-    mesh_poerator.Bonedot_OT_CutoffMesh,
-)
+def install_wheels():
+    addon_dir = os.path.dirname(__file__)
+    libs_dir = os.path.join(addon_dir, "lib")
+    wheels = [
+        os.path.join(libs_dir, f) for f in os.listdir(libs_dir) if f.endswith(".whl")
+    ]
+    subprocess.check_call([sys.executable, "-m", "ensurepip"])
+    for wheel_path in wheels:
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "install", "--upgrade", wheel_path]
+        )
+
+
+def get_classes():
+    from bone_dot.panel import viewport_panel, sprite_panel
+    from bone_dot.operator import view2d_operator, sprite_operator, mesh_poerator
+
+    classes = (
+        viewport_panel.Bonedot_PT_ViewPortPanel,
+        view2d_operator.Bonedot_OT_SetView2D,
+        # import sprite
+        sprite_panel.Bonedot_PT_ImportSprite,
+        sprite_operator.Bonedot_OT_ImportSprites,
+        sprite_operator.Bonedot_OT_CreateMaterialGroup,
+        sprite_operator.Bonedot_OT_ImportSingleSprite,
+        mesh_poerator.Bonedot_OT_CutoffMesh,
+    )
+    return classes
 
 
 def register():
+    install_wheels()
+    classes = get_classes()
     for cls in classes:
         bpy.utils.register_class(cls)
 
 
 def unregister():
+    classes = get_classes()
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
